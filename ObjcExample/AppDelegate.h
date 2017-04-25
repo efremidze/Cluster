7ba67185e2fb7e2ee4e01193c3e10d6539856e66
Aaron Brethorst <aaron@brethorsting.com>
2017-04-24 22:28:36 -0700

    Adds Objective-C sample project

---
+++ ObjcExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjcExample
+//
+//  Created by Aaron Brethorst on 4/24/17.
+//  Copyright © 2017 efremidze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjcExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjcExample
+//
+//  Created by Aaron Brethorst on 4/24/17.
+//  Copyright © 2017 efremidze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
