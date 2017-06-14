5c07eb192378c95dfb3cbc67794b16ea4f88daf4
Lasha Efremidze <lasha.efremidze@ring.com>
2017-06-14 11:34:28 -0700

    Create Cluster.swift

--- Sources/Cluster.swift
+++ Sources/Cluster.swift
@@ -97,7 +97,7 @@ open class ClusterManager {
      - Parameters:
         - mapView: The map view object to reload.
      */
-    open func reload(_ mapView: MKMapView, visibleMapRect: MKMapRect, complete: @escaping ()->() = {}) {
+    open func reload(_ mapView: MKMapView, visibleMapRect: MKMapRect, completion: @escaping (() -> Void)? = nil) {
         let operation = BlockOperation()
         operation.addExecutionBlock { [weak self, weak mapView] in
             guard let strongSelf = self, let mapView = mapView else { return }
@@ -107,7 +107,7 @@ open class ClusterManager {
                     guard let mapView = mapView else { return }
                     mapView.removeAnnotations(toRemove)
                     mapView.addAnnotations(toAdd)
-                    complete()
+                    completion?()
                 }
             }
         }
