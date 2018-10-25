67de8c996dc865cb9747c7d8d5fa246bb74bfda1
Lasha Efremidze <efremidzel@hotmail.com>
2018-10-24 20:48:27 -0700

    Updated weak self

--- Sources/Cluster.swift
+++ Sources/Cluster.swift
@@ -219,11 +219,11 @@ open class ClusterManager {
         let zoomScale = Double(mapBounds.width) / visibleMapRectWidth
         queue.cancelAllOperations()
         queue.addBlockOperation { [weak self, weak mapView] operation in
-            guard let `self` = self, let mapView = mapView else { return completion(false) }
+            guard let self = self, let mapView = mapView else { return completion(false) }
             autoreleasepool { () -> Void in
                 let (toAdd, toRemove) = self.clusteredAnnotations(zoomScale: zoomScale, visibleMapRect: visibleMapRect, operation: operation)
                 DispatchQueue.main.async { [weak self, weak mapView] in
-                    guard let `self` = self, let mapView = mapView else { return completion(false) }
+                    guard let self = self, let mapView = mapView else { return completion(false) }
                     self.display(mapView: mapView, toAdd: toAdd, toRemove: toRemove)
                     completion(true)
                 }
@@ -234,7 +234,7 @@ open class ClusterManager {
     open func clusteredAnnotations(zoomScale: Double, visibleMapRect: MKMapRect, operation: Operation? = nil) -> (toAdd: [MKAnnotation], toRemove: [MKAnnotation]) {
         var isCancelled: Bool { return operation?.isCancelled ?? false }
         
-        guard !isCancelled, !zoomScale.isInfinite, !zoomScale.isNaN else { return (toAdd: [], toRemove: []) }
+        guard !isCancelled else { return (toAdd: [], toRemove: []) }
         
         let mapRects = self.mapRects(zoomScale: zoomScale, visibleMapRect: visibleMapRect)
         
@@ -316,18 +316,17 @@ open class ClusterManager {
         return (toAdd: toAdd, toRemove: toRemove)
     }
     
-    func addOverlays(zoomScale: Double, visibleMapRect: MKMapRect) {
+    func addOverlays(visibleMapRect: MKMapRect, mapRects: [MKMapRect]) {
         overlays.removeAll()
         overlays.append(MKPolyline(mapRect: visibleMapRect))
-        
-        let mapRects = self.mapRects(zoomScale: zoomScale, visibleMapRect: visibleMapRect)
-        
         for mapRect in mapRects {
             overlays.append(MKPolyline(mapRect: mapRect))
         }
     }
     
     func mapRects(zoomScale: Double, visibleMapRect: MKMapRect) -> [MKMapRect] {
+        guard !zoomScale.isInfinite, !zoomScale.isNaN else { return [] }
+        
         zoomLevel = zoomScale.zoomLevel
         let scaleFactor = zoomScale / cellSize(for: zoomLevel)
         
