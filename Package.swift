1cb20cb1e256c13202a9c370558c2c7ebcdb41ba
Lasha Efremidze <efremidzel@hotmail.com>
2025-07-08 14:25:35 -0700

    Upgrade Swift Package (#143)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Cluster",
     platforms: [
-        .iOS(.v8)
+        .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
